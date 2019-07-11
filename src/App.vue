4c7ec99cdb81e8cb4e06b0e5845859a5d711a227
hejun <hej@southgnss.com>
2019-07-11 16:57:24 +0800

    第一个webgl

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,41 @@
 <template>
-  <div id = 'app'>
-    <el-container >
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <el-menu :default-openeds="['0',]">
-          <el-submenu index="i" v-for="(item,i) in aside" :key="i" >
-            <template slot="title">{{item.cate}}</template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1" v-for="(subItem,j) in item.items " :key="j">
-                <a href="#" @click="clickSideItem(subItem.title, subItem.com)">{{subItem.title}}</a>
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-        </el-menu>
-      </el-aside>
-      <el-container>
-        <el-header style="text-align: right; font-size: 12px">
-          <el-input disabled v-text="currentTitle" style="text-align: left; font-size: 20px;width: 70%;" ></el-input>
-          <el-dropdown>
-            <i class="el-icon-view" style="margin-right: 15px"></i>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
+  <el-container  id = 'app'>
+    <!--侧栏-->
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu :default-openeds="[0]">
+        <el-submenu  v-for="(item,i) in aside"  :index = 'item.cate'  :key="i" >
+          <template slot="title">{{item.cate}}</template>
+          <el-menu-item-group>
+            <el-menu-item v-for="(subItem,j) in item.items " :index = "subItem.com"  :key="j">
+              <a href="#" @click="clickSideItem(subItem.title, subItem.com)">{{subItem.title}}</a>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
 
-        </el-header>
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-input disabled v-text="currentTitle" style="text-align: left; font-size: 20px;width: 70%;" ></el-input>
+        <el-dropdown>
+          <i class="el-icon-view" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
 
-        <el-main>
-          <div :is = "filterComponent"></div>
-        </el-main>
-      </el-container>
+      </el-header>
+
+      <!--展示 main-->
+      <el-main :is = "filterComponent"></el-main>
     </el-container>
-  </div>
+
+
+  </el-container>
+
+
 </template>
 <script>
   import C1HelloWorld from './components/cesium/C1HelloWorld'
@@ -50,7 +53,7 @@
             cate:'webgl',
             items:
               [
-                {title: '1.webgl三角形',route: '1', com:'w1-hello-world'},
+                {title: '1.第一个 webgl', com:'w1-hello-world'},
                 // {title: '2.webgl平移',route: '1'}
               ]
           },
@@ -70,16 +73,16 @@
                 // {title: '2.cesium',route: '1'}
               ]
           },
-          {
-            cate:'babyicon',
-            items:
-                    [
-                      {title: '1.babyicon', com:'c1-hello-word'},
-                      // {title: '2.babyicon',route: '1'}
-                    ]
-          }
+          // {
+          //   cate:'babyicon',
+          //   items:
+          //           [
+          //             {title: '1.babyicon', com:'c1-hello-word'},
+          //             // {title: '2.babyicon',route: '1'}
+          //           ]
+          // }
         ],
-        currentTitle: '123',
+        currentTitle: '',
         filterComponent: ''
       }
     },
--- src/components/three/T1HelloWorld.vue
+++ src/components/three/T1HelloWorld.vue
@@ -1,13 +1,64 @@
 <template>
-    <div> three1111111</div>
+    <div ref="container" id="container"></div>
 </template>
 
 <script>
+    import * as THREE from 'three'
     export default {
-        name: 'T1HelloWorld'
+        name: 'T1HelloWorld',
+        data(){
+            return{
+                camera:null,
+                scene:null,
+                renderer: null,
+                geometry:null,
+                material:null,
+                mesh:null,
+                mainLight:null,
+                helper:null
+            }
+        },
+        methods:{
+            init(){
+                // 三要素  场景 灯光 相机 渲染
+                this.scene = new THREE.Scene();
+                this.mainLight =new THREE.DirectionalLight(0xFF0000)
+                // this.scene.add(this.mainLight)
+                this.camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,10000)
+                this.renderer =  new THREE.WebGLRenderer({antialias:true})
+                this.renderer.setSize(window.innerWidth,window.innerHeight)
+                this.renderer.setClearColor(0xffffff,1)
+                this.$refs.container.appendChild(this.renderer.domElement)
+                this.camera.position.z = 1
+            },
+            update() {
+                requestAnimationFrame(this.update)
+                this.animate()
+                this.renderer.render(this.scene, this.camera)
+            },
+            animate(){
+                this.mesh.rotation.x += 0.01
+                this.mesh.rotation.y += 0.01
+            },
+            addObject(){
+                this.geometry = new THREE.BoxGeometry(0.1,0.1,0.1)
+                this.material = new THREE.MeshBasicMaterial({color:0xff0000})
+                this.mesh = new THREE.Mesh(this.geometry , this.material)
+                this.scene.add(this.mesh)
+            }
+        },
+        mounted(){
+            this.$nextTick( () => {
+                this.init()
+                this.addObject()
+                this.update()
+            })
+        },
     }
 </script>
 
 <style scoped>
-
+ #container{
+     margin: 0;
+ }
 </style>
